92080dff4c7221669a4aa5c0fb664781f956adaa
tatsuyaagari <kanri.1217@gmail.com>
2017-08-09 07:08:44 +0000

    want

--- config/initializers/rakuten_web_service.rb
+++ config/initializers/rakuten_web_service.rb
@@ -1,3 +1,4 @@
-RakutenWebService. configuration do |c|
-  c.application_id = ENV['RAKUTEN_APPLICATION_ID=1056640422722047214']
+RakutenWebService.configuration do |c|
+  c.application_id = ENV['RAKUTEN_APPLICATION_ID']
 end
+
